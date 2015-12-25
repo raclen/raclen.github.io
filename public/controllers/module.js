6273618fa75faae63e51d0c1cbd26e827afb199a
dixiao <dixiao@Ctrip.com>
2015-12-25 13:39:52 +0800

    给wrapper设置高度，防止数据少不好看，压缩资源文件

--- public/controllers/module.js
+++ public/controllers/module.js
@@ -1,7 +1,1 @@
-/**
- * Created by dixiao on 2015/12/16.
- */
-define(['angular'], function (ng) {
-    'use strict';
-    return ng.module('app.controllers', []);
-});
+define(["angular"],function(ng){"use strict";return ng.module("app.controllers",[])});
