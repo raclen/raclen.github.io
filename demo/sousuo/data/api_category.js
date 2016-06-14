69303acb22632da17ae6f4a3a5b562baf33d17b8
dixiao <dixiao@Ctrip.com>
2016-06-14 16:45:13 +0800

    提交demo部分

---
+++ demo/sousuo/data/api_category.js
@@ -0,0 +1,62 @@
+/**
+ * Created by dixiao on 2015/11/5.
+ 1:number
+ 2:String
+ "isshowdropdownbox":1
+
+ */
+jsonp1([
+  {
+    "categoryId": 2014110601,
+    "categoryName": "资源大区",
+    "type": 1,
+    "isshowdropdownbox":1
+
+  },
+  {
+    "categoryId": 2014110602,
+    "categoryName": "城市",
+    "type": 2
+  },
+  {
+    "categoryId": 2014110603,
+    "categoryName": "省份",
+    "type": 2
+  },
+  {
+    "categoryId": 2014110604,
+    "categoryName": "主题标签",
+    "type": 2
+  }, {
+    "categoryId": 2014110605,
+    "categoryName": "品类",
+    "type": 2
+  },
+  {
+    "categoryId": 2014110606,
+    "categoryName": "可选项关键字",
+    "type": 2
+  },
+  {
+    "categoryId": 2014110607,
+    "categoryName": "票型",
+    "type": 2
+  },
+  {
+    "categoryId": 2014110608,
+    "categoryName": "毛利率",
+    "type": 1
+  },
+  {
+    "categoryId": 2014110609,
+    "categoryName": "国家",
+    "type": 2
+  },
+  {
+    "categoryId": 2014110610,
+    "categoryName": "售价",
+    "type": 1
+  }
+])
+
+
